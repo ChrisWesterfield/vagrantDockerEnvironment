186a8da1d658fb58164bbbf62c528409ebab0020
Chris Westerfield <chris@mjr.one>
2017-03-10 23:55:36 +0100

    Update vagrant/provision.sh

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -17,3 +17,23 @@ ln -sfn /vagrant/etc/sites-enabled/ /etc/nginx/sites-enabled
 
 /usr/sbin/service nginx restart
 
+
+apt-get install multitail mysql-client -y
+
+echo "127.0.0.1       dbMaster dbSlave php1 php2 nginx es elasticsearch queue memcached mailcatcher redis phpmyadmin statsd mongodb errbit" >> /etc/hosts
+
+apt-get install bash-builtins bash-completion -y
+
+echo "source /vagrant/.bash_profile" >> /home/vagrant/.bashrc
+
+apt-get install haveged -y
+
+echo "adding backports"
+
+echo "deb http://ftp.debian.org/debian jessie-backports main" > /etc/apt/sources.list.d/backports.list
+
+apt-get update
+apt-get upgrade -y
+
+
+apt-get -t jessie-backports install linux-image-amd64
